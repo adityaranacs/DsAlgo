f3b4d9c1fa3a00438bef12295b25e0c689a89405
Aditya rana <94234121+adityaranacs@users.noreply.github.com>
2022-03-08 12:17:02 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    int c = a + b;
+    return c;
+}
+
+
+
+
+int main()
+{
+    // cout<<"hello world"<<endl;
+    // cout<<"hi aditya";
+    // int a, b,c;
+    // short sa=9;
+    // cout<<sa;
+
+    // camel case notation
+    // int marksInMaths=83;
+    // cout<<"marks of student in maths="<<marksInMaths;
+
+    // short a;
+    // int n;
+    // long c;
+    // long long d;
+    // float score = 45.44;   //if we use float const score then score will not cahge regardless
+    // double score2 = 45.455;
+    // long double score3=4.555;
+    // score = 34.3;
+    // cout<<"The score is=" <<score;
+
+    // User input
+
+    // int a, b;
+    // cout<<"Enter 1st Number:"<<endl;
+    // cin>>a;
+
+    // cout<<"Enter s2nd Number:"<<endl;
+    // cin>>b;
+
+    // cout<<" a + b is " <<a + b<<endl;
+    // cout<<" a - b is " <<a - b<<endl;
+    // cout<<" a * b is " <<a * b<<endl;
+    // cout<<" a / b is " <<(float) a / b<<endl;
+
+    // if else condition
+
+    // int age;
+    // cout << "Enter your agre" << endl;
+    // cin >> age;
+    // if (age>=18){
+    //     cout<<"You can vote";
+    // }
+    //     else
+    //      if(age>150){
+    //         cout<<"Invalid age";
+
+    //     }
+
+    //     else{
+    //         cout<<"You cannot vote";
+    //     }
+
+    // switch (age)
+    // {
+    // case 12:
+    //     cout << "Your age is 12";
+    //     break;
+
+    // case 18:
+    //     cout << "Your age is 18";
+    //     break;
+
+    // default:
+    //     cout << "Neither";
+
+    //     break;// not necesaary taht break is her
+    // }
+
+
+
+
+
+
+// loops
+
+
+//  int index =0;
+// while(index<34)
+// {
+//     cout<<"We are at index"<<index<<endl;
+//     index = index + 1;
+
+
+
+// do 
+// {
+//   cout<<"We are at index"<<index<<endl;  
+//   index = index + 1;
+// }
+// while (index );
+
+
+// for loop
+
+// for ( int i = 1; i <=34; i++)
+// {
+//     cout<<"The value of indes is  "<<i<<endl;
+// }
+
+
+
+
+
+
+
+
+// functions
+
+
+
+
+
+
+
+    return 0;
+}
